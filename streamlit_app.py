438fa3410dffd7602269c8dd68a5c0df225039d4
Jyoti0710 <112945840+Jyoti0710@users.noreply.github.com>
2022-09-06 19:49:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,7 @@ import streamlit
 streamlit.title('My Parents new Healthy Dinner')
    
     
-BreakFast Menu
+streamlit.header('BreakFast Menu')
+streamlit.text('omega3 & Blueberry oatmeal')
+streamlit.text('kale spinach & rocket smootie')
+streamlit.text('Hard_boiled  Free_range eggs')
